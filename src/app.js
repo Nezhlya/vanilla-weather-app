ad4c2de89c7a1f9b313bade1c65f4fde69b57c06
Nezhlya <122394560+Nezhlya@users.noreply.github.com>
2023-11-08 12:21:18 +0000

    forecast not correct

--- src/app.js
+++ src/app.js
@@ -1,79 +1,48 @@
-let apiKey = "b5e7d88b0d219a3822897ddaeda253c9";
+
+const apiKey = "b5e7d88b0d219a3822897ddaeda253c9";
 let units = "metric";
 
-let dateElement = document.querySelector("#timeDate");
-let date = moment().format("dddd H:mm");
-dateElement.innerHTML = date;
+const dateElement = document.querySelector("#timeDate");
+const forecastElement = document.querySelector("#forecast");
 
-let forecast = document.querySelector("#forecast");
-function showWeekdays(timestamp) {
-  let date = new Date(timestamp * 1000);
-  let day = date.getDay();
-  let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
-  return days[day];
-}
-function showForecast(response) {
-  console.log(response);
-  let forecastDaily = response.data.daily;
-  let forecastHTML = ` <div class="row row-cols-3" id="head">`;
-  forecastDaily.forEach(function (forecastDay, index) {
-    if (index < 6) {
-      forecastHTML =
-        forecastHTML +
-        ` <div class="col">${showWeekdays(forecastDay.dt)}</div>
-    <div class="col"> <img
-          src="http://openweathermap.org/img/wn/${
-            forecastDay.weather[0].icon
-          }@2x.png"
-          alt=""
-          width="42"
-        /></div>
-  <div class="col"><span id="high">${Math.round(
-    forecastDay.temp.max
-  )}°  </span><span id="low">${Math.round(forecastDay.temp.min)}°</span></div>
-    `;
-    }
-  });
-  forecast.innerHTML = forecastHTML;
-  forecastHTML = forecastHTML + `</div>`;
-}
+const showWeekdays = timestamp => ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"][new Date(timestamp * 1000).getDay()];
 
-function getForecast(coordinates) {
-  let apiUrl = `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=${units}`;
-  axios.get(apiUrl).then(showForecast);
-}
-function enterCity(city) {
-  apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${apiKey}`;
-  axios.get(apiUrl).then(showWeather);
-}
-function enterInput(event) {
+const showForecast = response => {
+  console.log(response)
+  const forecastDaily = Object.entries(response.data); 
+  if (!Array.isArray(forecastDaily)) {
+    console.error("Invalid forecast data");
+    return;}
+  const forecastHTML = forecastDaily.map(forecastDay => `
+    <div class="col">${showWeekdays(forecastDay.dt)}</div>
+    <div class="col">
+      <img src="http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png" alt="" width="42" />
+    </div>
+    <div class="col">
+      <span id="high">${Math.round(forecastDay.temp_max)}°</span>
+      <span id="low">${Math.round(forecastDay.temp_min)}°</span>
+    </div>
+  `).join("");
+  forecastElement.innerHTML = `<div class="row row-cols-3" id="head">${forecastHTML}</div>`;
+};
+
+const getWeatherData = city => {
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${apiKey}`;
+  axios.get(apiUrl)
+    .then(response => {
+      showWeather(response);
+      showForecast(response); 
+    })
+};
+
+const enterCity = event => {
   event.preventDefault();
-  let city = document.querySelector("input.city").value;
-  let nameCity = document.querySelector("#userCity");
-  nameCity.innerHTML = city.charAt(0).toUpperCase() + city.slice(1);
+  const city = document.querySelector("input.city").value;
+  document.querySelector("#userCity").innerHTML = city.charAt(0).toUpperCase() + city.slice(1);
+  getWeatherData(city);
+};
 
-  enterCity(city);
-}
-
-let form = document.querySelector("#searchCity");
-form.addEventListener("submit", enterInput);
-
-function showWeather(response) {
-  console.log(response);
-  let card = document.querySelector("div.container").style;
-  card.position = "absolute";
-  card.display = "grid";
-  card.justifyItems = "center";
-  card.justifyContent = "space-evenly";
-  card.gridTemplateAreas = `"header header header header"
-    "main1 . . main2"`;
-  let header = document.querySelector("#header").style;
-  header.gridArea = "header";
-  let main = document.querySelector("#today").style;
-  main.gridArea = "main1";
-  let forecast = document.querySelector("#forecast").style;
-  forecast.gridArea = "main2";
-
+const showWeather = response => {
   let temp = document.querySelector("#temp");
   let celsius = response.data.main.temp;
   temp.innerHTML = Math.round(celsius);
@@ -118,23 +87,22 @@ function showWeather(response) {
       essential: true,
     });
   });
-  getForecast(response.data.coord);
-}
-function toCelsius(event) {
+  
+   showForecast(response)
+  $("span.units").show();
+  $("#units").show();
+};
+
+const changeUnits = (event, newUnits) => {
   event.preventDefault();
-  document.querySelector("#units").innerHTML = "m/s";
-  units = "metric";
-}
+  units = newUnits;
+  const city = document.querySelector("input.city").value;
+  getWeatherData(city);
+};
 
-function toFahrenheit(event) {
-  event.preventDefault();
-  document.querySelector("#units").innerHTML = "mph";
-  units = "imperial";
-}
+document.querySelector("#searchCity").addEventListener("submit", enterCity);
+document.querySelector("#celsius").addEventListener("click", event => changeUnits(event, "metric"));
+document.querySelector("#fahrenheit").addEventListener("click", event => changeUnits(event, "imperial"));
 
 
-let celsiusTof = document.querySelector("#fahrenheit");
-celsiusTof.addEventListener("click", toFahrenheit);
 
-let fahrenheiToc = document.querySelector("#celsius");
-fahrenheiToc.addEventListener("click", toCelsius);
