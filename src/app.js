1ceff689c6abeb1d513666e18695a20e19f13115
Nezhlya <122394560+Nezhlya@users.noreply.github.com>
2023-11-09 12:14:34 +0000

    weekays not correct

--- src/app.js
+++ src/app.js
@@ -7,32 +7,39 @@ const forecastElement = document.querySelector("#forecast");
 
 const showWeekdays = timestamp => ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"][new Date(timestamp * 1000).getDay()];
 
-const showForecast = response => {
-  console.log(response)
-  const forecastDaily = Object.entries(response.data); 
-  if (!Array.isArray(forecastDaily)) {
-    console.error("Invalid forecast data");
-    return;}
-  const forecastHTML = forecastDaily.map(forecastDay => `
-    <div class="col">${showWeekdays(forecastDay.dt)}</div>
-    <div class="col">
-      <img src="http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png" alt="" width="42" />
-    </div>
-    <div class="col">
-      <span id="high">${Math.round(forecastDay.temp_max)}°</span>
-      <span id="low">${Math.round(forecastDay.temp_min)}°</span>
-    </div>
-  `).join("");
-  forecastElement.innerHTML = `<div class="row row-cols-3" id="head">${forecastHTML}</div>`;
-};
+  const showForecast = response => {
+    console.log(response)
+    const forecastDaily = response.data.list;
+
+    const forecastHTML = forecastDaily.slice(0, 6).map(forecastDay => {
+      const timestamp = forecastDay.dt * 1000; 
+      const dayOfWeek = new Date(timestamp).toLocaleDateString("en-US", { weekday: "short" });
+  
+      return `
+        <div class="col">${dayOfWeek}</div>
+        <div class="col">
+          <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="" width="42" />
+        </div>
+        <div class="col">
+          <span id="high">${Math.round(forecastDay.main.temp_max)}°</span>
+          <span id="low">${Math.round(forecastDay.main.temp_min)}°</span>
+        </div>
+      `;
+    }).join("");
+  
+    forecastElement.innerHTML = `<div class="row row-cols-3" id="head">${forecastHTML}</div>`;
+  };
+  
+
 
 const getWeatherData = city => {
-  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${apiKey}`;
+  const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=${units}&appid=${apiKey}`;
+
   axios.get(apiUrl)
     .then(response => {
       showWeather(response);
-      showForecast(response); 
-    })
+      showForecast(response);
+    });
 };
 
 const enterCity = event => {
@@ -44,51 +51,50 @@ const enterCity = event => {
 
 const showWeather = response => {
   let temp = document.querySelector("#temp");
-  let celsius = response.data.main.temp;
+  let celsius = response.data.list[0].main.temp;
   temp.innerHTML = Math.round(celsius);
 
   let humidity = document.querySelector("#humidity");
-  humidity.innerHTML = `<b>Humidity</b> ${response.data.main.humidity}%`;
+  humidity.innerHTML = `<b>Humidity</b> ${response.data.list[0].main.humidity}%`;
 
   let wind = document.querySelector("#wind");
-  wind.innerHTML = `<b>Wind speed</b> ${Math.round(response.data.wind.speed)}`;
+  wind.innerHTML = `<b>Wind speed</b> ${Math.round(response.data.list[0].wind.speed)}`;
 
   let descr = document.querySelector("#description");
-  descr.innerHTML = response.data.weather[0].description;
+  descr.innerHTML = response.data.list[0].weather[0].description;
 
   let weatherIcon = document.querySelector("#icon");
-  let iconCode = response.data.weather[0].icon;
+  let iconCode = response.data.list[0].weather[0].icon;
   weatherIcon.setAttribute(
     "src",
     `http://openweathermap.org/img/wn/${iconCode}@2x.png`
   );
-  weatherIcon.setAttribute("alt", response.data.weather[0].main);
+  weatherIcon.setAttribute("alt", response.data.list[0].weather[0].main);
 
   $("span.units").show();
   $("#units").show();
 
-  //map
-
+  // Map
   maptilersdk.config.apiKey = "uWIkkOudLeqGl9dH8rgx";
   var map = new maptilersdk.Map({
     container: "map",
     style: maptilersdk.MapStyle.CITIES,
-    center: [response.data.coord.lon, response.data.coord.lat],
+    center: [response.data.city.coord.lon, response.data.city.coord.lat],
     zoom: 7,
   });
 
   let marker = new maptilersdk.Marker()
-    .setLngLat([response.data.coord.lon, response.data.coord.lat])
+    .setLngLat([response.data.city.coord.lon, response.data.city.coord.lat])
     .addTo(map);
 
   document.getElementById("src").addEventListener("click", function () {
     map.flyTo({
-      center: [response.data.coord.lon, response.data.coord.lat],
+      center: [response.data.city.coord.lon, response.data.city.coord.lat],
       essential: true,
     });
   });
-  
-   showForecast(response)
+
+  showForecast(response);
   $("span.units").show();
   $("#units").show();
 };
