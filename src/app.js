56a0c26643bc4ff28a188909eb5a693c6d8ccd89
Nezhlya <122394560+Nezhlya@users.noreply.github.com>
2023-11-20 09:25:57 +0000

    forecast correct

--- src/app.js
+++ src/app.js
@@ -5,24 +5,30 @@ let units = "metric";
 const dateElement = document.querySelector("#timeDate");
 const forecastElement = document.querySelector("#forecast");
 
-const showWeekdays = timestamp => [ "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"][new Date(timestamp * 1000).getDay()];
+const showWeekdays = timestamp => ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"][new Date(timestamp * 1000).getDay()];
 
-  const showForecast = response => {
-    console.log(response)
-    let forecastDaily = response.data.list
-    const forecastHTML = forecastDaily.slice(0, 6).map(forecastDay => `
-    <div class="col">${showWeekdays(forecastDay.dt)}</div>
-    <div class="col">
-      <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="" width="42" />
-    </div>
-    <div class="col">
-      <span id="high">${Math.round(forecastDay.main.temp_max)}°</span>
-      <span id="low">${Math.round(forecastDay.main.temp_min)}°</span>
-    </div>
-  `).join("");
+const showForecast = response => {
+  console.log(response);
+  let forecastDaily = response.data.list;
+  const uniqueDays = [...new Set(forecastDaily.map(forecastDay => new Date(forecastDay.dt * 1000).getDay()))];
+  const forecastHTML = uniqueDays.map(day => {
+    const dayData = forecastDaily.find(forecastDay => new Date(forecastDay.dt * 1000).getDay() === day);
+    return `
+      <div class="col">${showWeekdays(dayData.dt)}</div>
+      <div class="col">
+        <img src="http://openweathermap.org/img/wn/${dayData.weather[0].icon}@2x.png" alt="" width="42" />
+      </div>
+      <div class="col">
+        <span id="high">${Math.round(dayData.main.temp_max)}°</span>
+        <span id="low">${Math.round(dayData.main.temp_min)}°</span>
+      </div>
+    `;
+  }).join("");
+
   forecastElement.innerHTML = `<div class="row row-cols-3" id="head">${forecastHTML}</div>`;
-  };
-  
+};
+
+
 
 
 const getWeatherData = city => {
